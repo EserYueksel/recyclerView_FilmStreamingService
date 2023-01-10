b7ef6bc15753607846bf03ec9418ff17911f5920
EserYueksel <eseryuksel@live.de>
2023-01-10 14:32:16 +0100

    recyclerView built with AndroidStudio within SyntaxInstitute course

---
+++ app/src/main/java/de/syntax_institut/filmestreamingservice/data/model/Movie.kt
@@ -0,0 +1,8 @@
+package de.syntax_institut.filmestreamingservice.data.model
+
+/**
+ * Diese Klasse repräsentiert einen Filmtitel
+ */
+data class Movie(val stringresource: Int, val imageresource: Int){
+
+}
